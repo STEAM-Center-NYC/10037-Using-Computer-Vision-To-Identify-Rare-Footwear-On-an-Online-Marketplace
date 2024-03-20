85af52e0d807fae7ac745de3a81337f576e78968
Djames16 <djames@brooklynsteamcenter.org>
2024-03-20 10:26:36 -0400

    and main

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ from pprint import pprint as print
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from dynaconf import Dynaconf
+import PIL
+import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split # as something ez to write pls
+import tensorflow as tf
+import tensorflow_hub as hub
+import os
+import matplotlib.pyplot as pplt
+import matplotlib.image as ppltimg
 
 ######
 
